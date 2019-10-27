1d201aafd41da7356caaafde93051bf32c686966
JGBJunior <jgbjunior99@gmail.com>
2019-10-27 23:31:49 +0100

    <numeros>

---
+++ unity/Assets/numeros.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class numeros : MonoBehaviour {
+
+	public int numeroUno;
+	public int numeroDos;
+	public int numeroTres;
+	public float numeroCuatro;
+	public float numeroCinco;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		Debug.Log ("El resultado de multiplicar " + numeroUno + " por " + numeroDos + ", dividido por " + numeroTres + ", es igual a " + (numeroUno * numeroDos) / numeroTres );
+		Debug.Log ("El resultado de sumar " + numeroUno + ",mas" + numeroTres + ",mas" + numeroCuatro + ", es igual a" + numeroUno + numeroTres + numeroDos);
+		Debug.Log ("El resultado de restar " + numeroUno + ",menos" + numeroDos + ",menos" + numeroCinco + ", es igual a" + (numeroUno - numeroDos - numeroCinco));
+		Debug.Log ("El resultado de multiplicar " + numeroUno + ",mas" + numeroDos + ",por" + numeroCinco + ", es igual a" + (numeroUno + numeroDos) * numeroCinco);
+		Debug.Log ("El resultado de calcular " + numeroUno + ",mas" + numeroDos + ",dividido por " + numeroCinco + ",mas" + numeroCuatro + ", es igual a" + (numeroUno - numeroTres) / (numeroCinco + numeroCuatro));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
